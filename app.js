ca026d6650c958b7ddd7402e01f2642f1fd5894e
Wojciech Berezowski <61703808+wkberezowski@users.noreply.github.com>
2022-04-21 12:36:23 +0200

    finished project

--- app.js
+++ app.js
@@ -7,10 +7,37 @@ const end = section.querySelector('h1');
 // scrollmagic
 const controller = new ScrollMagic.Controller();
 
-const scene = new ScrollMagic.Scene({
+let scene = new ScrollMagic.Scene({
   duration: 9000,
   triggerElement: intro,
   triggerHook: 0,
 })
   .setPin(intro)
   .addTo(controller);
+
+// text animation
+const textAnim = TweenMax.fromTo(text, 3, { opacity: 1 }, { opacity: 0 });
+
+let scene2 = new ScrollMagic.Scene({
+  duration: 3000,
+  triggerElement: intro,
+  triggerHook: 0,
+})
+  .setTween(textAnim)
+  .addTo(controller);
+
+// video animation
+
+let accelamount = 0.1;
+let scrollpos = 0;
+let delay = 0;
+
+scene.on('update', (e) => {
+  scrollpos = e.scrollPos / 1000;
+});
+
+setInterval(() => {
+  delay += (scrollpos - delay) * accelamount;
+
+  video.currentTime = delay;
+}, 33.3);
